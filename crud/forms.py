fc2bf1366f3fd1364220fc6e2dbcb919d1acfeee
Kauan Carolino <kauancarolino248@gmail.com>
2023-12-03 14:42:56 -0300

    Primeira commit

---
+++ crud/forms.py
@@ -0,0 +1,9 @@
+# from django.forms import forms
+# from crud.models import Cliente
+
+# class Form(forms.ModelForm):
+#     class Meta:
+#         model = Cliente
+#         fields = ('nome', 'idade')
+
+
---
+++ crud/views.py
@@ -0,0 +1,7 @@
+# from django.shortcuts import render
+# from django.views.generic import ListView
+# from crud.models import Cliente
+
+# class ClienteList(ListView):
+#     model = Cliente
+#     queryset = Cliente.object.all()
